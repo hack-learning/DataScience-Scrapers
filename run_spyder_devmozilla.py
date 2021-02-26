de8ef73aa4265f739a2aeae11dbbce9ae4c42ab5
juanjtov <juanjosetmontana@gmail.com>
2021-02-25 20:23:17 -0800

    Cleaning the branches 2

--- run_spyder_devmozilla.py
+++ run_spyder_devmozilla.py
@@ -2,6 +2,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from news_scraper.spiders.devmozilla import MozillaDev
 
+#running the mozilla scraper
 process = CrawlerProcess(get_project_settings())
 process.crawl(MozillaDev)
 process.start()
