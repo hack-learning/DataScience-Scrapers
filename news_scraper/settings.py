9e6d27b8b98ee332c1f279e3a9793263cbba9018
Alex Chirino <alexchirino3@gmail.com>
2021-07-18 00:04:30 -0500

    Fix typo

--- news_scraper/settings.py
+++ news_scraper/settings.py
@@ -64,7 +64,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'news_scraper.pipelines.DataBasePipeline':100,
+    'news_scraper.pipelines.DataBasePipeline': 100,
     'news_scraper.pipelines.NewsScraperPipeline': 300,
 }
 
